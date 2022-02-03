86b04fb006cadcbec8275b2557697651d39438f4
JuchokJuk <juk.juchok@yandex.ru>
2022-02-03 22:57:56 +0300

    cloudinary fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,5 @@
 module.exports = [
+  // ...
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
@@ -8,4 +9,19 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // ...
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,18 @@
 module.exports = ({ env }) => ({
+    // ...
     upload: {
+      config: {
         provider: 'cloudinary',
         providerOptions: {
             cloud_name: env('do8kqfk4d'),
             api_key: env('539647757755573'),
             api_secret: env('wDR8zbisOMV22KyCSDZg-PjW1d0'),
         },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
     },
-});
+    // ...
+  });
